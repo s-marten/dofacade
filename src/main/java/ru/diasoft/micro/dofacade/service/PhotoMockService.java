2a58d8108dfd811789e4cc60c8f2be878d4dce5c
Vitaly Pashichev <pasvitas1@gmail.com>
2020-03-19 13:57:08 +0300

    Логирование инициализированных сервисов

--- src/main/java/ru/diasoft/micro/dofacade/service/PhotoMockService.java
+++ src/main/java/ru/diasoft/micro/dofacade/service/PhotoMockService.java
@@ -1,8 +1,12 @@
 package ru.diasoft.micro.dofacade.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import ru.diasoft.micro.dofacade.model.RecognitionResult;
+
+import javax.annotation.PostConstruct;
 
 @Service
 @ConditionalOnProperty(
@@ -10,7 +14,13 @@ import ru.diasoft.micro.dofacade.model.RecognitionResult;
         havingValue = "mock",
         matchIfMissing = true
 )
+@Slf4j
 public class PhotoMockService implements IPhotoService {
+
+    @PostConstruct
+    private void logAfterInit() {
+        log.info("Started Photo Mock Service");
+    }
 
     @Override
     public RecognitionResult comparePhotos(byte[] photo1, byte[] photo2) {
--- src/main/java/ru/diasoft/micro/dofacade/service/PhotoService.java
+++ src/main/java/ru/diasoft/micro/dofacade/service/PhotoService.java
@@ -1,8 +1,11 @@
 package ru.diasoft.micro.dofacade.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import ru.diasoft.micro.dofacade.model.RecognitionResult;
+
+import javax.annotation.PostConstruct;
 
 //TODO: Сюда подрубать реальные апихи
 @Service
@@ -10,7 +13,13 @@ import ru.diasoft.micro.dofacade.model.RecognitionResult;
         value="photoService",
         havingValue = "ntechlab"
 )
+@Slf4j
 public class PhotoService implements IPhotoService {
+
+    @PostConstruct
+    private void logAfterInit() {
+        log.info("Started NTechLab Photo service");
+    }
 
     @Override
     public RecognitionResult comparePhotos(byte[] photo1, byte[] photo2) {
